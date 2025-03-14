3d90cd82c1fad7d6d2a0d73b68e60fdffe756bfe
Mladen Stanimirovic <mstanimirovic@proton.me>
2025-03-14 13:04:57 +0100

    Adds selection sort algorithm
    adds default values to utils functions

--- arrays/selection_sort.py
+++ arrays/selection_sort.py
@@ -1,1 +1,30 @@
-# todo
+
+from utils import generate_random_array, display_array
+
+
+def selection_sort(arr, order = lambda x, y: x > y):
+    """
+    Selection Sort Algorithm
+
+    Args:
+        arr (list): The array to be sorted.
+        order (callable): A function that takes two elements and returns True if the first element should come before the second element in the sorted array.
+
+    Returns:
+        None: The function sorts the array in-place.
+    """
+    n = len(arr);
+    for i in range(n):
+        key_index = i
+        for j in range(i + 1, n):
+            if order(arr[key_index], arr[j]):
+                key_index = j
+        arr[key_index], arr[i] = arr[i], arr[key_index]
+
+
+if __name__ == "__main__":
+    arr = generate_random_array(10, 1, 100)
+    display_array(arr, "[ORIGINAL] = ")
+
+    selection_sort(arr, lambda x, y: x < y)
+    display_array(arr, "[SORTED] = ")
--- arrays/utils.py
+++ arrays/utils.py
@@ -1,8 +1,8 @@
 
 import random
 
-def generate_random_array(size, min_val, max_val):
+def generate_random_array(size = 10, min_val = 0, max_val = 100):
     return [random.randint(min_val, max_val) for _ in range(size)]
 
-def display_array(arr, text):
+def display_array(arr, text = ""):
     print(text, arr, sep="")
