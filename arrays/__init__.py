f314067024b7e3878ef6c36c88e6669e41383830
Mladen Stanimirovic <mstanimirovic@proton.me>
2025-03-14 13:36:33 +0100

    Add QuickSort implementation and init exports

---
+++ arrays/__init__.py
@@ -0,0 +1,8 @@
+
+from .bubble_sort import bubble_sort
+from .insertion_sort import insertion_sort
+from .merge_sort import merge_sort
+from .quick_sort import quick_sort
+from .selection_sort import selection_sort
+
+__all__ = ['bubble_sort', 'insertion_sort', 'merge_sort', 'quick_sort', 'selection_sort']
--- arrays/quick_sort.py
+++ arrays/quick_sort.py
@@ -1,1 +1,42 @@
-# todo
+
+from utils import generate_random_array, display_array
+
+
+def quick_sort(arr):
+    """
+    Quick Sort Algorithm
+
+    Args:
+        arr (list): The array to be sorted.
+
+    Returns:
+        list: The sorted array.
+    """
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[-1]
+    left = []
+    same = []
+    right = []
+
+    for el in arr:
+        if el == pivot:
+            same.append(el)
+        elif el > pivot:
+            right.append(el)
+        else:
+            left.append(el)
+
+    left_arr = quick_sort(left)
+    right_arr = quick_sort(right)
+
+    return left_arr + same + right_arr
+
+
+if __name__ == "__main__":
+    arr = generate_random_array()
+    display_array(arr, "[ORIGINAL] = ")
+
+    sorted_arr = quick_sort(arr)
+    display_array(sorted_arr, "[SORTED] = ")
